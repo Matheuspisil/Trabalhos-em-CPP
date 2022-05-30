fce89ba794ec37639e48d990b26348c275846788
Matheus Pires e Silva <67922748+Matheuspisil@users.noreply.github.com>
2022-05-30 19:10:18 -0300

    Autômato definição por idade
    Digite a idade do competidor em potencial o programa indicará qual categoria o competidor ira pertencer.

---
+++ competidos divididos por classes.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+// trabalho simples de faculdade 2018//
+{
+
+  int idade;
+
+  cout <<"Idade do competidor: ";
+
+  cin >>idadde;
+
+  if (idade>=7 && idade<=12)
+
+  {
+
+    cout <<"Categoria: INFANTIL";
+
+  }
+
+  else if (idade>=13 && idade<=17)
+
+  {
+
+    cout <<"Categoria: JUVENIL";
+
+  }
+
+  else if (idade>=18 && idade<=49)
+
+  {
+
+    cout <<"Categoria: ADULTO";
+
+  }
+
+  else if (idade>=50)
+
+  {
+
+    cout <<"Categoria: SENIOR";
+
+  }
+
+  else
+
+  {
+
+    cout <<"Idade inexistente!";
+
+  }
+
+}
